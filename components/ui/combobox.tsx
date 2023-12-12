a0e1735e7c953457e393e6a9402439d57213bcf4
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2023-12-12 21:37:04 +0700

    Update combobox.tsx

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { Check, ChevronsUpDown } from "lucide-react";
 
@@ -22,63 +24,49 @@ interface ComboboxProps {
   onChange: (value: string) => void;
 }
 
-// eslint-disable-next-line react/display-name
-export const Combobox = React.forwardRef<HTMLButtonElement, ComboboxProps>(
-  ({ options, value, onChange }, ref) => {
-    const [open, setOpen] = React.useState(false);
+export const Combobox = ({ options, value, onChange }: ComboboxProps) => {
+  const [open, setOpen] = React.useState(false);
 
-    const handleButtonClick = () => {
-      setOpen(!open);
-      if (!open && ref.current) {
-        ref.current.focus();
-      }
-    };
-
-    const handleSelectOption = (optionValue: string) => {
-      onChange(optionValue === value ? "" : optionValue);
-      setOpen(false);
-    };
-
-    return (
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            ref={ref}
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            onClick={handleButtonClick}
-            className="w-full justify-between"
-          >
-            {value
-              ? options.find((option) => option.value === value)?.label
-              : "Select option..."}
-            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-full p-0">
-          <Command>
-            <CommandInput placeholder="Search option..." />
-            <CommandEmpty>No option found.</CommandEmpty>
-            <CommandGroup>
-              {options.map((option) => (
-                <CommandItem
-                  key={option.value}
-                  onSelect={() => handleSelectOption(option.value)}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === option.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {option.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
-    );
-  }
-);
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full justify-between"
+        >
+          {value
+            ? options.find((option) => option.value === value)?.label
+            : "Select option..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-full p-0">
+        <Command>
+          <CommandInput placeholder="Search option..." />
+          <CommandEmpty>No option found.</CommandEmpty>
+          <CommandGroup>
+            {options.map((option) => (
+              <CommandItem
+                key={option.value}
+                onSelect={() => {
+                  onChange(option.value === value ? "" : option.value);
+                  setOpen(false);
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    value === option.value ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {option.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
